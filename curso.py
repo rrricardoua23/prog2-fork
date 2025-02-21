2d1467b9d7894ae44492d809e6e4e0a2b8db871a
carlacarreras <ccv51@alu.ua.es>
2025-02-21 13:40:07 +0100

    Update curso.py

--- curso.py
+++ curso.py
@@ -6,3 +6,16 @@ class Curso:
         self.codigo = codigo
         self.descripcion = descripcion
         Curso.total_cursos += 1  # Aumenta el contador cada vez que se crea un curso
+    def mostrar_detalles(self):
+        pass
+    def actualizar_descripcion(self,nueva_descripcion:str):
+        pass
+    def __str__(self):
+        pass
+    def __repr__(self):
+        pass
+    def desde_tupla(self,cls,tupla):
+        pass
+    def es_curso_abierto(self):
+        pass
+

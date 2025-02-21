4b1dae26429adc75013e75b52a97ce5401cad5e9
Saraperezperaza <smpp4@alu.ua.es>
2025-02-21 13:39:10 +0100

    Update estudiante.py

--- estudiante.py
+++ estudiante.py
@@ -6,3 +6,20 @@ class Estudiante:
         self.edad = edad
         self.cursos_inscritos = cursos_inscritos
         Estudiante.total_estudiantes += 1  # Aumenta el contador cada vez que se crea un estudiante
+    def inscribir_curso(self,curso):
+        pass
+    def mostrar_informacion(self):
+        pass
+    def __str__(self):
+        pass
+    def __repr__(self):
+        pass
+    @classmethod
+    def desde_tuplas(self, cls, tupla):
+        pass
+    @staticmethod
+    def es_mayor_de_edad(self):
+        pass
+    
+
+
